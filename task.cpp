cac5a5c702c6e865458bfccb06b8aa8c0b2a88bb
Ommi Sandeep <322103382043@gvpce.ac.in>
2024-10-05 12:18:07 +0530

    added task.cpp

---
+++ task.cpp
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+int main()
+{
+	int a;
+	printf("Enter any value :");
+	scanf("%d",&a);
+	if(a>0)
+	printf("It is positive number");
+	else if(a<0)
+	printf("It is negative number");
+	else
+	printf("It is Zero");
+	
+	return 0;
+}
+
